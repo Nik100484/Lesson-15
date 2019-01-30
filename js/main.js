f6270e4516390ed4b7f0e7a956da34f9ce7fb883
Nikolay Sukhorada <niksuhoy@gmail.com>
2019-01-30 23:22:00 +0300

    les 15

---
+++ js/main.js
@@ -0,0 +1,64 @@
+var message = 'No pasaran';
+console.log('No pasaran');
+
+var num = 158-72;
+console.log(num);
+
+
+var a = true; 
+var b = 5 > 9; 
+console.log( b ); 
+
+var moto = ["Suzuki", "Honda", "Kawasaki"];
+console.log(moto[0]);
+
+var myObject = {
+	name: "John",
+	age: 45
+};
+console.log(myObject);
+
+var number = 5.59529;
+console.log(number.toFixed(2));
+
+var i = 10;
+i++;
+console.log(i);
+
+var i = 10;
+i--;
+console.log(i);
+
+var n = 5;
+n += 2;
+console.log(n);
+n -= 3;
+console.log(n);
+n *= 218;
+console.log(n);
+n /= 5;
+console.log(n);
+
+var t = 200;
+t %= 30;
+console.log(t);
+
+console.log(Math.PI);
+console.log(Math.round(89.279));
+console.log(Math.random() * 10);
+console.log(Math.pow(2,5));
+
+var string = "A new report  on the state of American";
+console.log(string.length);
+
+var string = "A new report on the state of American";
+console.log(string.substring(7));
+
+var string = "A new report on the state of American";
+console.log(string.replace("American", "Africa"));
+
+var string = "A new report on the state of American";
+console.log(string.toUpperCase());
+
+var string = "A new report on the state of American";
+console.log(string.toLowerCase());
